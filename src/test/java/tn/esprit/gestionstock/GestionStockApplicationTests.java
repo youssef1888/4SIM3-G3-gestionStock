3276a67dd6503358c3cb070dc39557183466f846
yousef hammami <mongihammami13@gmail.com>
2025-12-15 11:36:00 +0100

    Initialisation du projet Spring Boot

---
+++ src/test/java/tn/esprit/gestionstock/GestionStockApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
