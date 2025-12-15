93d6709ef7f14241d84ec54be69c521c444aafb1
yousef hammami <mongihammami13@gmail.com>
2025-12-15 12:05:53 +0100

    Ajout tests unitaires JUnit

--- src/main/java/tn/esprit/gestionstock/GestionStockApplication.java
+++ src/main/java/tn/esprit/gestionstock/GestionStockApplication.java
@@ -1,13 +1,18 @@
 package tn.esprit.gestionstock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionStockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionStockApplication.class, args);
-	}
+	private static final Logger log = LoggerFactory.getLogger(GestionStockApplication.class);
 
+	public static void main(String[] args) {
+		log.info("Démarrage de l'application GestionStock");
+		SpringApplication.run(GestionStockApplication.class, args);
+		log.debug("Application GestionStock démarrée avec succès");
+	}
 }
--- src/test/java/tn/esprit/gestionstock/GestionStockApplicationTests.java
+++ src/test/java/tn/esprit/gestionstock/GestionStockApplicationTests.java
@@ -3,11 +3,20 @@ package tn.esprit.gestionstock;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class GestionStockApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
+	@Test
+	void simpleTest() {
+		int a = 2 + 3;
+		assertEquals(5, a);
+	}
 }
